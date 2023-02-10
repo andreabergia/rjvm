21c21c46e36cbca8d93f62288c745aa219e22f9d
Andrea Bergia <andrea.bergia@lastminute.com>
2023-02-10 10:12:42 +0100

    feat: extend support to versions until Java 17

--- src/reader/class_file_version.rs
+++ src/reader/class_file_version.rs
@@ -8,8 +8,19 @@ pub enum ClassFileVersion {
     Jdk1_3,
     Jdk1_4,
     Jdk1_5,
-    #[default]
     Jdk6,
+    Jdk7,
+    Jdk8,
+    Jdk9,
+    Jdk10,
+    Jdk11,
+    Jdk12,
+    Jdk13,
+    Jdk14,
+    Jdk15,
+    Jdk16,
+    #[default]
+    Jdk17,
 }
 
 impl ClassFileVersion {
@@ -21,6 +32,17 @@ impl ClassFileVersion {
             48 => Ok(ClassFileVersion::Jdk1_4),
             49 => Ok(ClassFileVersion::Jdk1_5),
             50 => Ok(ClassFileVersion::Jdk6),
+            51 => Ok(ClassFileVersion::Jdk7),
+            52 => Ok(ClassFileVersion::Jdk8),
+            53 => Ok(ClassFileVersion::Jdk9),
+            54 => Ok(ClassFileVersion::Jdk10),
+            55 => Ok(ClassFileVersion::Jdk11),
+            56 => Ok(ClassFileVersion::Jdk12),
+            57 => Ok(ClassFileVersion::Jdk13),
+            58 => Ok(ClassFileVersion::Jdk14),
+            59 => Ok(ClassFileVersion::Jdk15),
+            60 => Ok(ClassFileVersion::Jdk16),
+            61 => Ok(ClassFileVersion::Jdk17),
             _ => Err(ClassReaderError::UnsupportedVersion(major, minor)),
         }
     }
@@ -43,8 +65,8 @@ mod tests {
     #[test]
     fn can_parse_future_versions() {
         assert_eq!(
-            Err(ClassReaderError::UnsupportedVersion(62, 65535)),
-            ClassFileVersion::from(62, 65535),
+            Err(ClassReaderError::UnsupportedVersion(99, 65535)),
+            ClassFileVersion::from(99, 65535),
         );
     }
 }
